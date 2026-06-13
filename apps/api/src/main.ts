68bb05c2da628369f5624be5f8340bd00d8c4f2f
Jupiter <kjimin2123@gmail.com>
2026-06-13 18:59:58 +0900

    feat(api): add Swagger configuration and update dependencies
    - Swagger 설정을 추가하여 API 문서화를 지원합니다.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import { SwaggerModule } from '@nestjs/swagger';
+import swaggerConfig from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory());
 
   app.use(helmet());
   app.use(cookieParser());

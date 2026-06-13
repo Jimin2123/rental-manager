dab6c0a9004fb973681393c50ec823aecb6822fa
Jupiter <kjimin2123@gmail.com>
2026-06-13 19:25:47 +0900

    feat(api): add configuration module and validation pipe
    - ConfigModule을 추가하여 환경 변수를 전역적으로 설정합니다.
    - ValidationPipe를 사용하여 요청 유효성 검사를 강화합니다.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import { SwaggerModule } from '@nestjs/swagger';
 import swaggerConfig from './configs/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
   const documentFactory = () =>
     SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, documentFactory());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   app.use(helmet());
   app.use(cookieParser());

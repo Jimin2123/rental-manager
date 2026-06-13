d955ec204c75ac713d4bd94f78f40b8f95a907de
Jupiter <kjimin2123@gmail.com>
2026-06-13 18:42:16 +0900

    feat(api): add cookie-parser for enhanced request handling
    - cookie-parser를 추가하였습니다

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
